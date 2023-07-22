d12ff8209a73bf4ba5ff2011a9aa0c9cce912a63
huangjian561 <95408188+huangjian561@users.noreply.github.com>
2023-07-22 06:06:04 +0000

    updata

--- app.py
+++ app.py
@@ -1,10 +1,39 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
-
+JOBS = [
+  {
+    'id':'1',
+    'title':'test demo 1',
+    'location':'china jiangshu',
+    'salary':'CNY 1000'
+  },
+  {
+    'id':'2',
+    'title':'test demo 2',
+    'location':'china jiangshu',
+    'salary':'CNY 1000'
+  },
+  {
+    'id':'3',
+    'title':'test demo 3',
+    'location':'china jiangshu',
+    'salary':'CNY 1000'
+  },
+  {
+    'id':'4',
+    'title':'test demo 4',
+    'location':'china jiangshu',
+    'salary':'CNY 1000'
+  }
+]
 @app.route("/")
 def Hello_World():
-  return render_template('home.html')
+  return render_template('home.html', jobs=JOBS)
+
+@app.route("/jobs")
+def jobs_list():
+  return jsonify(JOBS)
 if __name__ == '__main__':
   app.run('0.0.0.0', debug=True)
